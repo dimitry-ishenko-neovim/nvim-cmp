9acc878348f4c518a6a05aa492168896eb3d5ed6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2025-07-11 23:22:14 -0400

    Add nvim_lsp_signature_help as source

--- debian/cmp.lua
+++ debian/cmp.lua
@@ -49,6 +49,7 @@ cmp.setup({
 
     sources = cmp.config.sources(
         {{name = "copilot"}, {name = "nvim_lsp"}, {name = "snippy"}},
+        {{name = "nvim_lsp_signature_help"}},
         {{name = "buffer"}}
     )
 })
